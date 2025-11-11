886e1f00798787fb3ecd80d4bf3eb9dfcb29bb29
uimss <uimss.imagelabo@gmail.com>
2025-11-12 02:06:04 +0900

    ゲーム一覧とちゅうまで

---
+++ src/data/navLinks.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { url: '/about', title: 'About' },
+  { url: '/illustrations', title: 'Illustrations' },
+  { url: '/games', title: 'Games' },
+  { url: 'https://uimss.booth.pm/', title: 'Shop' },
+];
