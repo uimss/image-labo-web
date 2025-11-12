fdb2161e57ef2f27e794d89e8a6689eed2f0c9e5
uimss <uimss.imagelabo@gmail.com>
2025-11-13 02:50:20 +0900

    commissionedの分とちゅうまで

--- src/data/illustrations.ts
+++ src/data/illustrations.ts
@@ -1,5 +1,22 @@
 export interface Illustration {
+  image: string;
   description: string;
-  url?: string;
   publishDate: string;
+  url?: string;
+
 }
+
+export const illustrations: Illustration[] = [
+  {
+    image: "tsukinoyo_202505.png",
+    description: "「バブル（Cover）」 MVイラスト - 月の夜、izu",
+    publishDate: "2025.9.26",
+    url: "https://www.nicovideo.jp/watch/sm45443011"
+  },
+  {
+    image: "sou_202507.png",
+    description: "「ブルースクリーン」 MVイラスト - Sou",
+    publishDate: "2025.9.26",
+    url: "https://www.nicovideo.jp/watch/sm45443011"
+  }
+]
